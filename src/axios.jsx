bbe6f5523c17b1aa5e19a85e05e1e669424d7b48
Sathish292004 <sathishsince2004@gmail.com>
2026-06-22 23:50:18 +0530

    commit

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://e-com-backend-eufc.onrender.com",
+  baseURL: "https://e-com-backend-eufc.onrender.com/api",
 });
 delete API.defaults.headers.common["Authorization"];
 export default API;
